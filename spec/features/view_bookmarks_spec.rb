c78fcc47f0109d342287d89678e117b9c6267d36
Will Dixon <dixon.will1@gmail.com>
2020-12-15 15:08:30 +0000

    pg with expected failure

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,6 @@
 feature 'viewing bookmarks' do
   scenario 'should present all existing bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.twitter.com"
+    expect(page).to have_content "www."
   end
 end
